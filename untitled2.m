bb384bd0b1ee901a59c7ebd17f1105ff07cdcd04
kadecl <yashima777wille@gmail.com>
2024-01-31 20:34:07 +0900

    modify the graph whch I used in the paper

--- untitled2.m
+++ untitled2.m
@@ -1,7 +1,7 @@
 figure
-subplot(2,1,1)
 semilogy(axisFreq, abs(fft(sbi + sbi_harm  + nmix + tmix + 0.2*org_high)));
+hold on;
+semilogy(axisFreq, abs(fft(lowpass(sbi+nWR+tOLA,fs/4,fs))))
 xlim([50, fs/2])
-subplot(2,1,2); semilogy(axisFreq, abs(fft(lowpass(sbi+nWR+tOLA,fs/4,fs))))
-xlim([50, fs/2])
+legend("提案法", "既存手法")
 xlabel("周波数 (Hz)")
